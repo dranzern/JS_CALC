f1d463a7369997645ce95eb814242cfacb7c3242
Daniel Jesus Ayala Rivera <danielbardero@gmail.com>
2026-09-17 22:45:19 -0600

    Update miScript.js

--- JS_CALC/miScript.js
+++ JS_CALC/miScript.js
@@ -1,47 +1,88 @@
-function recomendar(genero) {
-    let elementoEdad = document.getElementById("ingresarEdad").value;
-    let elementoPelicula = document.getElementById("mostrarPelicula");
-    
-    switch(genero){
-        case 'comedia' :
-            if (elementoEdad < 13 ) {
-                elementoPelicula.textContent = "Son como ninos";
-            } else  {
-                if (elementoEdad < 16) {
-                    elementoPelicula.textContent = "Supercool";
-                } else {
-                    elementoPelicula.textContent = "Proyecto X";
-                } 
-            }
-            break;
-        case 'drama' :
-            if (elementoEdad < 13) {
-                elementoPelicula.textContent = "Titanic";
-            }else {
-                if (elementoEdad < 16) {
-                    elementoPelicula.textContent = "Diario de una pasion";
-                } else {
-                    elementoPelicula.textContent = "Cleopatra vol.3"
-                }
-            }
-            break;
-        case 'musical' :
-            if (elementoEdad < 13){
-                elementoPelicula.textContent = "Highschool musical";
-            } else if (elementoEdad < 16) {
-                elementoPelicula.textContent = "Mama mia";
-            } else {
-                elementoPelicula.textContent = "Los miserables";
-            } 
-            break; 
-        case 'crimen' :
-            if (elementoEdad < 13) {
-                elementoPelicula.textContent = "Pareja explosiva";
-            } else if (elementoEdad < 16) {
-                elementoPelicula.textContent = "Crimen padre amado";
-            } else {
-                elementoPelicula.textContent = "Amanecer rojo";
-            }
-            break;
-    }
+
+function mostrarResultado(resultado) {
+    document.getElementById("campo3").value = resultado;
+}
+
+
+function sumar() {
+    let valor1 = +document.getElementById("campo1").value;
+    let valor2 = +document.getElementById("campo2").value;
+    let operacion = valor1 + valor2;
+
+    mostrarResultado(operacion);
+} 
+
+function restar() {
+    let valor1 = +document.getElementById("campo1").value;
+    let valor2 = +document.getElementById("campo2").value;
+    let operacion = valor1 - valor2;
+
+    mostrarResultado(operacion);
+}
+
+function multiplicar() {
+    let valor1 = +document.getElementById("campo1").value;
+    let valor2 = +document.getElementById("campo2").value;
+    let operacion = valor1 * valor2;
+
+    mostrarResultado(operacion);
+}
+
+function dividir() {
+    let valor1 = +document.getElementById("campo1").value;
+    let valor2 = +document.getElementById("campo2").value;
+    let operacion = valor1 / valor2;
+
+    mostrarResultado(operacion);
+}
+
+function raiz() {
+    let valor2 = +document.getElementById("campo2").value;
+    let operacion = Math.sqrt(valor2);
+
+    mostrarResultado(operacion);
+}
+
+function potencia() {
+    let valor1 = +document.getElementById("campo1").value;
+    let valor2 = +document.getElementById("campo2").value;
+    let operacion = Math.pow(valor1 , valor2);
+
+    mostrarResultado(operacion);
+}
+
+function valorAbsoluto() {
+     let valor2 = +document.getElementById("campo2").value;
+     let operacion = Math.abs(valor2);
+
+    mostrarResultado(operacion);
+}
+
+function numeroAleatorio() {
+    let valor1 = +document.getElementById("campo1").value;
+    let valor2 = +document.getElementById("campo2").value;
+    let operacion = Math.random()*(valor2 - valor1 + 1) + valor1;
+
+    mostrarResultado(operacion);
+}
+
+function redondeo() {
+    let valor3 = +document.getElementById("campo3").value;
+    let operacion = Math.round(valor3);
+
+    mostrarResultado(operacion);
+}
+
+function redondeoAbajo() {
+    let valor3 = +document.getElementById("campo3").value;
+    let operacion = Math.floor(valor3);
+
+    mostrarResultado(operacion);
+}
+
+function redondeoArriba() {
+    let valor3 = +document.getElementById("campo3").value;
+    let operacion = Math.ceil(valor3);
+
+    mostrarResultado(operacion);
 }
